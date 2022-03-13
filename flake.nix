ea8ab11088cd0dc9a6c53bea4360602975d4a83e
Regis Kuckaertz <regiskuckaertz@users.noreply.github.com>
2022-03-13 13:48:19 +0000

    Pin Nix dependencies

--- flake.nix
+++ flake.nix
@@ -2,14 +2,22 @@
   description = "Le site web de Paul Valensier. Oui, *THE* Paul Valensier";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixkpgs/nixos-21.11;
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, nixpkgs, flake-utils }: 
+    let 
+      forSystem = system: 
+        let 
+          pkgs = nixpkgs.legacyPackages.${system};
+        in 
+          {
+            devShell.x86_64-darwin =
+              pkgs.mkShell {
+                buildInputs = [ pkgs.nodejs ];
+              };
+          };
+    in
+      flake-utils.lib.eachDefaultSystem forSystem;
 }
